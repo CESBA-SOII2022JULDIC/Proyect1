33c9f493fdc1f1e49edef21787a4d7fd6925ff2e
Berna019 <2011364@cesba-queretaro.edu.mx>
2022-08-27 08:45:58 -0500

    Commit inicial del proyecto

---
+++ src/java/Universidades.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Administrador
+ */
+public class Universidades {
+    private int id;
+    private String nombre;
+    private String status;
+}
